42bd845d5fdd24b874b5b6440f2dc850cb6c27cf
Cheryl Daniels <cheryld433@gmail.com>
2020-03-28 16:59:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,148 @@
 // Variables  
 var passwordlength = 0;
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = lowerCase.toUpperCase();
-var numbers = "1234567890";
-var specChar = "!#$%&'()*+,-./:;?@][^_`{|}~'<=>";
+var confirmLowerCase = false;
+var confirmUpperCase = false;
+var confirmNumber = false;
+var confirmSpecChar = false;
 
-//Password Length
-var passwordlength = parseInt(prompt("Enter length of password. Number must be between 8-128 characters"));
+var lowerCase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+var upperCase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specChar = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+function collectUserPasswordChoice(){
+  //Password Length
+ passwordlength = parseInt(prompt("Enter length of password. Number must be between 8-128 characters"));
 
 //Must input a number
 while (isNaN(passwordlength)) {
-  var passwordlength = parseInt(prompt("This is not a number. Please enter a number between 8 - 128."));
+   passwordlength = parseInt(prompt("This is not a number. Please enter a number between 8 - 128."));
 } 
 //Password stipulations
 while (passwordlength < 8 || passwordlength > 128){
-  var passwordlength = parseInt(prompt("Enter length of password. Number must be between 8-128 characters"));
+   passwordlength = parseInt(prompt("Enter length of password. Number must be between 8-128 characters"));
 } 
 // Confirm user input (lowercase.uppercase,numbers and special characters.)
-var confirmLowerCase = confirm("Would you like to include lower case letters?");
-var confirmUpperCase = confirm("Would you like to include upper case letters?");
-var confirmNumber = confirm("Would you include to use numbers?");
-var confirmSpecChar = confirm("Would you like to include special characters?");
+ confirmLowerCase = confirm("Would you like to include lower case letters?");
+ confirmUpperCase = confirm("Would you like to include upper case letters?");
+ confirmNumber = confirm("Would you include to use numbers?");
+ confirmSpecChar = confirm("Would you like to include special characters?");
 
 // generate password
-var userPassword = "";
-generatePassword();
-alert("Your Password is: "+ userPassword);
-
+var newPassword = generatePassword();
+console.log('newPassword: ' + newPassword);
 //Write generated password on page
-document.getElementById("password").innerHTML = userPassword;
+document.getElementById("password").innerHTML = newPassword;
+}
+
 
 //Generate password
-var passwordGroup = "";
+
 function generatePassword() {
-  if (confirmLowerCase) {
-  passwordGroup += lowerCase;
-  }
-  if (confirmUpperCase) {
-  passwordGroup += upperCase;
-  }
-  if (confirmNumber) {
-  passwordGroup += numbers;
-  }
-  if (confirmSpecChar) {
-  passwordGroup += specChar;
-  }
-  //for loop
+  var passwordGroup = "";
+
   for (let i = 0; i < passwordlength; i++) {
-    userPassword += passwordGroup.charAt(
-    Math.floor(Math.random() * passwordGroup.length)
-    );
+    if (confirmLowerCase && passwordGroup.length < passwordlength) {
+      passwordGroup += getRandom(lowerCase);
+      }
+      if (confirmUpperCase && passwordGroup.length < passwordlength) {
+      passwordGroup += getRandom(upperCase);
+      }
+      if (confirmNumber && passwordGroup.length < passwordlength) {
+      passwordGroup += getRandom(numbers);
+      }
+      if (confirmSpecChar && passwordGroup.length < passwordlength) {
+      passwordGroup += getRandom(specChar);
+      }
   }
-  return userPassword;
+  return passwordGroup;
 }
+
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
 //Copy the password to clipboard
 function copyPassword(){
   var copyText = document.getElementById("password");
@@ -61,3 +151,7 @@ function copyPassword(){
   document.execCommand("copy");
   alert("Copied");
 }
+
+
+var btnGenerate = document.querySelector('#generate');
+btnGenerate.addEventListener('click', collectUserPasswordChoice)
